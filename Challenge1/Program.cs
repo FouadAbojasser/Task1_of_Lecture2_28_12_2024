7080aa06597bd28468a7b3ad6dad15fb067ff3d3
fouadabujasser <fouad.abujasser@gmail.com>
2025-01-03 23:19:07 +0200

    Add project files.

---
+++ Challenge1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Challenge1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            List<int> myList = new List<int>();
+            Console.WriteLine("Please Enter Any Value ... ");
+            myList.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine($"myList Count = {myList.Count}");
+            Console.WriteLine($"myList Capacity = {myList.Capacity}");
+
+            Console.WriteLine("Please Enter Any Value ... ");
+            myList.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine($"myList Count = {myList.Count}");
+            Console.WriteLine($"myList Capacity = {myList.Capacity}");
+
+
+            Console.WriteLine("Please Enter Any Value ... ");
+            myList.Append(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine($"myList Count = {myList.Count}");
+            Console.WriteLine($"myList Capacity = {myList.Capacity}");
+
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double smallCarpetPrice = 25;
+            Double largeCarpetPrice = 35;
+            Double tax = (6);
+
+            DateTime offerStartingDate = new DateTime(2025, 1, 1);
+            DateTime offerEndDate = offerStartingDate.AddDays(30);
+
+            Console.WriteLine("=== Welcome to Islam Carpet Cleaner ===");
+            Console.WriteLine("============================================");
+            Console.WriteLine($"Price for cleaning small carpet = {smallCarpetPrice}$ ");
+            Console.WriteLine($"Price for cleaning large carpet = {largeCarpetPrice}$ ");
+            Console.WriteLine($"Added Taxes = {tax}% ");
+            Console.WriteLine("============================================");
+
+            Console.Write("Please enter number of small carpets: ");
+            int smallCarpets = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter number of large carpets: ");
+            int largeCarpets = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("============================================");
+            Console.WriteLine($"Cost for cleaning your small carpet(s) = {smallCarpets * smallCarpetPrice}$ ");
+            Console.WriteLine($"Cost for cleaning your large carpet(s) = {largeCarpets * largeCarpetPrice}$ ");
+            Double totalCost = (smallCarpets * smallCarpetPrice) + (largeCarpets* largeCarpetPrice);
+            Console.WriteLine("----------");
+            Console.WriteLine($"Cleaning cost = {totalCost}$");
+            Console.WriteLine("----------");
+            Console.WriteLine($"Taxes = {(totalCost * tax)/100}$");
+            Console.WriteLine("----------");
+            Console.WriteLine($"Total order Cost = {totalCost + ((totalCost * tax) / 100)}$");
+            Console.WriteLine("============================================");
+            Console.WriteLine($"The offer is valid for {(offerEndDate - DateTime.Now).Days} more days.");
+            Console.WriteLine("============================================");
+
+
+
+
+
+        }
+    }
+}
